20a59ddc15b7a51c3c0aeccc47ede5b1eda7c93c
RODRIGO STRAMANTINOLI <rodrigostramantinoli@gmail.com>
2020-09-17 08:32:57 -0300

    exibe a foto do usuario na tela "sua conta"

--- Office/Controllers/UsuariosController.cs
+++ Office/Controllers/UsuariosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,15 +11,35 @@ namespace Office.Controllers
     public class UsuariosController : Controller
     {
         private readonly Contexto _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public UsuariosController(Contexto context)
+        public UsuariosController(Contexto context, IWebHostEnvironment hostEnvironment)
         {
+            webHostEnvironment = hostEnvironment;
             _context = context;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _context.Usuarios.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["CaminhoFoto"] = webHostEnvironment.WebRootPath;
+            return View(user);
         }
     }
 }
